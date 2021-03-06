67aae29bb89021f8cae1c919217301df47ea69fd
panosku <zzpzaf@gmail.com>
2021-03-06 17:24:35 +0200

    ViewChilde-OK

--- src/app/country-crud/country-crud.component.ts
+++ src/app/country-crud/country-crud.component.ts
@@ -1,7 +1,7 @@
 
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Country } from '../dataclasses/country';
-
+import { ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-country-crud',
@@ -17,14 +17,57 @@ export class CountryCrudComponent implements OnInit {
   @Output() emitAddCountry: EventEmitter<Country> =   new EventEmitter<Country>();
   @Output() emitUpdCountry: EventEmitter<Country> =   new EventEmitter<Country>();
   @Output() emitDelCountry: EventEmitter<number> =   new EventEmitter<number>();
-  
-  
+ 
+  @ViewChild('newcid') i_id: any; // accessing the reference element
+  @ViewChild('newcname') i_name: any;
+  @ViewChild('newccapital') i_capital: any;
+  @ViewChild('newcregion') i_region: any;
+  @ViewChild('newcsubregion') i_subregion: any;
+  @ViewChild('newcpopulation') i_population: any;
+  @ViewChild('newcarea') i_aerea: any;
+  @ViewChild('newcalpha2code') i_alpha2code: any;
+  @ViewChild('newcalpha3code') i_alpha3code: any;
+  @ViewChild('newcgdp') i_gdp: any;
+
+
   
   constructor() { }
 
   ngOnInit(): void {
     //this.ccname = this.nc_name;
   }
+
+
+  ngOnChanges(): void {
+
+ /*    
+    We got a situation where initial values of form input fields can not be displayed 
+    after we have cleared them in clsForm(). This issue is quite strange and affects only
+    values that are repidetely the same - not all fields with different values
+    Using ViewChilde (or ViewChildren) with
+    template variables (#...) solved the issue.
+    We can start referring to a ViewChild property (variable) after the template/view 
+    has been initialized and and rendered. 
+    Normally, we can use them inside the ngAfterViewInit life-cycle Hook. 
+    However, this works just once. But we want to update the values each time the view 
+    is updated with data received e.g. from its parent component.
+    So, we can use them here, inside the ngOnChanges() hook, upon we check them are true, 
+    i.e. they are not undefined. 
+ */
+    if (this.i_region) this.i_region.nativeElement.value = this.nc_country.region ;
+    if (this.i_subregion) this.i_subregion.nativeElement.value = this.nc_country.subregion ;
+    
+    //console.log(this.nc_country.id, this.nc_country.region)
+
+
+  }
+
+
+
+
+
+
+
 
 
   // CRUD - Create a new Country
@@ -111,6 +154,24 @@ export class CountryCrudComponent implements OnInit {
     //return n;
     return false;
   }
+
+  clsForm() : boolean {
+    this.i_id.nativeElement.value = null;
+    this.i_name.nativeElement.value = ' ';
+    this.i_capital.nativeElement.value = ' ';
+    this.i_region.nativeElement.value = ' ';
+    this.i_subregion.nativeElement.value = ' ';
+    this.i_population.nativeElement.value =null;
+    this.i_aerea.nativeElement.value =null;
+    this.i_alpha2code.nativeElement.value = ' ';
+    this.i_alpha3code.nativeElement.value = ' ';
+    this.i_gdp.nativeElement.value = null;
+    return false;
+  }
+
+
+
+
 
 
 }
