575125da4dcf6bbdd488af33d09e21d92d33165b
panosku <zzpzaf@gmail.com>
2021-03-13 11:14:00 +0200

    HttpClient_Get_Remote_PHP_REST_API_Data_and_passed_them_into_other_components_Use_RxJs_Obervable_1st_time

--- src/app/app-info/app-info.component.ts
+++ src/app/app-info/app-info.component.ts
@@ -1,5 +1,7 @@
 import { DatePipe } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { CountriesTableComponent } from '../countries-table/countries-table.component';
+import { Country } from '../dataclasses/country';
 
 
 @Component({
@@ -9,8 +11,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppInfoComponent implements OnInit {
 
+  @ViewChild(CountriesTableComponent, { static: true })
+  ct_child!: CountriesTableComponent;
   
-
+  ARcountry: Country = new Country();
+  ARcountries: Country[] = [];
 
   title: string = 'This is a Countries information Application';
   info: string = '';
@@ -42,5 +47,33 @@ export class AppInfoComponent implements OnInit {
   }
 
 
+  UpdRemoteCountries(rcountries: Country[]) {
+
+    //console.log(rcountries);
+    //console.log("xxxxxxxxxxxx");
+
+    //this.ARcountries = [];
+    this.ARcountries = rcountries;
+    //console.log(this.ARcountries);
+
+    this.ct_child.remoteCountriesUpdated(this.ARcountries);
+
+  }
+
+
+  getLocal(b:boolean) {
+    this.ct_child.getLocalCountries();
+  }
+
+
+
+
+
+
+
+
+
+
 
 }
+
--- src/app/remoteapicountries1/remoteapicountries1.component.ts
+++ src/app/remoteapicountries1/remoteapicountries1.component.ts
@@ -1,45 +1,85 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Country } from '../dataclasses/country';
 import { Restphpapi1Service } from '../restphpapi1.service';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-remoteapicountries1',
   templateUrl: './remoteapicountries1.component.html',
-  styleUrls: ['./remoteapicountries1.component.css']
+  styleUrls: ['./remoteapicountries1.component.css','../country-crud/country-crud.component.css']
 })
 export class Remoteapicountries1Component implements OnInit {
+  @Output() emitRcountries: EventEmitter<Country[]> =   new EventEmitter<Country[]>();
+  @Output() emitLocalCountriess: EventEmitter<boolean> =   new EventEmitter<boolean>();
 
-  Rcountries: any = [];
+  Rcountry: Country = new Country();
+  Rcountries: Country[] = [];
+
+  headers: any;
 
   constructor(private myphpapi: Restphpapi1Service) { }
 
   ngOnInit(): void {
   
+    // this.myphpapi.getCountries()
+    // .subscribe(data => {
+    //   const dd: any = data;
+
+    //   for (const d of (dd.Countries as any)) {
+    //     this.Rcountries.push({
+    //       id: d.qid,
+    //       name: d.qname,
+    //       capital: d.qcapital,
+    //       region: d.qregion,
+    //       subregion: d.qsubregion,
+    //       population: d.qpopulation,
+    //       area: d.qarea,
+    //       code1: d.qalpha2code,
+    //       code2: d.qalpha3code,
+    //       gdp: d.qgdb
+    //     });
+    //   }
+
+    //   console.log(this.Rcountries);
+
+    // });
+
+  }
+
+  getRemoteData(): void {
+
+    console.log("------oBSERVABLES aGAIN !!!------");
+
     this.myphpapi.getCountries()
-    .subscribe(data => {
-      const dd: any = data;
+    .subscribe(resp => {
+      
+      //console.log(resp);
 
-      for (const d of (dd.Countries as any)) {
+      const keys = resp.headers.keys();
 
-        this.Rcountries.push({
-          id: d.qid,
-          name: d.qname,
-          capital: d.qcapital,
-          region: d.qregion,
-          subregion: d.qsubregion,
-          population: d.qpopulation,
-          area: d.qarea,
-          code1: d.qalpha2code,
-          code2: d.qalpha3code,
-          gdp: d.qgdb
-
-
-        });
+      this.headers = keys.map(key =>
+      `${key}: ${resp.headers.get(key)}`);
+      
+      const rbody: any = resp.body;
+      //console.log(rbody); 
+      this.Rcountries = [];  
+      for (const data of rbody.Countries as Country[]) {
+        this.Rcountries.push(data);
       }
 
-      console.log(this.Rcountries);
+      //console.log(this.Rcountries);
+      this.emitRcountries.emit(this.Rcountries);
 
     });
 
+  }
+
+
+
+  getLocalData(){
+    let getlocal: boolean = true;
+    this.emitLocalCountriess.emit(getlocal);
   }
 
 
